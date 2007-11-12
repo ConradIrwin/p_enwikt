7a0d51ac1c95a3c90477748e2a9260ced0f54e62
hippietrail <hippietrail@5d201f8b-831d-0410-bf51-e7b6101be352>
2007-11-12 13:35:50 +0000

    Pre-svn version 7
    git-svn-id: https://svn.toolserver.org/svnroot/p_enwikt@8 5d201f8b-831d-0410-bf51-e7b6101be352

--- wiktparser/wiktparser.pl
+++ wiktparser/wiktparser.pl
@@ -3,10 +3,30 @@
 
 use strict;
 
+#my ($lang_code, $lang_pat, $lang_g) = ('ang', '^Old English$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('ca', '^Catalan$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('cs', '^Czech$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('da', '^Danish$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('de', '^German$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('el', '^Greek$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('fr', '^French$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('he', '^Hebrew$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('is', '^Icelandic$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('lv', '^Latvian$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('nl', '^Dutch$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('pl', '^Polish$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('ru', '^Russian$', [] );
+my ($lang_code, $lang_pat, $lang_g) = ('sk', '^Slovak(?:ian)?$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('sl', '^Sloven(?:e|ian)$', [] );
+#my ($lang_code, $lang_pat, $lang_g) = ('sv', '^Swedish$', [] );
+
 my @scope;
 
-my $maxpages = 100;
+my $maxpages = 0;
 my $pagecounter = 0;
+
+my $tried_to_parse = 0;
+my $parsed_ok = 0;
 
 my %langnamestocodes;
 my %namespaces;
@@ -19,11 +39,11 @@ while ($xline = <STDIN>) {
 }
 
 if ($xline !~ /<namespaces>/) {
-	print STDERR "namespaces section not found\n";
+	print STDERR "** namespaces section not found\n";
 	exit;
 }
 
-print STDERR "found namespaces\n";
+print STDERR "** found namespaces\n";
 
 while ($xline = <STDIN>) {
 	if ($xline =~ /<namespace key="-?\d+"(.*)>/) {
@@ -37,11 +57,11 @@ while ($xline = <STDIN>) {
 }
 
 if ($xline !~ /<\/namespaces>/) {
-	print STDERR "end of namespaces section not found\n";
+	print STDERR "** end of namespaces section not found\n";
 	exit;
 }
 
-print STDERR "namespaces done\n";
+print STDERR "** namespaces done\n";
 
 my $nsre = '^(' . join('|', 'webster 1913', keys %namespaces) . '):(.*)$';
 
@@ -60,7 +80,7 @@ while (1) {
 	}
 
 	if ($xline !~ /<title>.*<\/title>/) {
-		print STDERR "no more pages";
+		print STDERR "** no more pages";
 		exit;
 	}
 
@@ -79,34 +99,35 @@ while (1) {
 
 	# If we ever had an article record without a text record
 	if ($xline =~ /<title>.*<\/title>/) {
-		print STDERR "$title has no text field\n";
+		print STDERR "** $title has no text field\n";
 		exit;
 	}
 
 	if ($xline !~ /<text xml:space="preserve">/) {
-		print STDERR "unexpected end of file while looking for $title's text field\n";
+		print STDERR "** unexpected end of file while looking for $title's text field\n";
 		exit;
 	}
 
 	# Language code template?
-	if ($ns eq 'Template') {
-		if ($title =~ /^(?:lang:)?([a-z][a-z][a-z]?)$/) {
-			my $langcode = $1;
+	#if ($ns eq 'Template') {
+	#	if ($title =~ /^(?:lang:)?([a-z][a-z][a-z]?)$/) {
+	#		my $langcode = $1;
 
-			if ($xline =~ /<text xml:space="preserve">(.*?)&lt;noinclude&gt;\[\[Category:Language templates|$title]]&lt;\/noinclude&gt;<\/text>/) {
-				my $langname = $1;
-				if ($langname =~ /^\[\[(?:.*\|)?(.*?)]]$/) {
-					$langname = $1;
-				}
+	#		if ($xline =~ /<text xml:space="preserve">(.*?)&lt;noinclude&gt;\[\[Category:Language templates|$title]]&lt;\/noinclude&gt;<\/text>/) {
+	#			my $langname = $1;
+	#			if ($langname =~ /^\[\[(?:.*\|)?(.*?)]]$/) {
+	#				$langname = $1;
+	#			}
 
-				if ($langname ne '') {
-					push @{$langnamestocodes{$langname}}, $langcode;
-				}
-			}
-		}
+	#			if ($langname ne '') {
+	#				push @{$langnamestocodes{$langname}}, $langcode;
+	#			}
+	#		}
+	#	}
 
 	# Article wikitext?
-	} elsif ($ns eq '') {
+	#} elsif ($ns eq '') {
+	if ($ns eq '') {
 
 		$pagecounter++;
 
@@ -118,24 +139,22 @@ while (1) {
 
 		@scope[0] = $page->{raw};
 
-		my $tline;
+		my $tline;					# line of wikitext to be parsed
 
+		my $prevsection;
 		my $section;				# each heading starts a new section
 
 		my $entry;					# each language heading starts a new entry
 
-		my $prevsection;
-
 		$page->{title} = $title;
 
 		$section = {};
-		my $level = 1;
-		$section->{level} = $level;
+		$section->{level} = 1;
 		$section->{heading} = $title;
 		$section->{lines} = [];
 		$section->{sections} = [];
 
-		$prevsection = appendsection($section, $level);
+		$prevsection = appendsection($section);
 
 		# Each line of page wikitext
 		while (1) {
@@ -169,15 +188,6 @@ while (1) {
 						$lang = $langname;
 					}
 					#push @{$page->{langs}}, $lang . ':' . $level;
-					$entry = {};
-					if ($langname eq 'English') {
-						$entry->{isen} = 1;
-						$page->{hasen} = 1;
-					} elsif ($langname eq 'Spanish') {
-						$entry->{ises} = 1;
-						$page->{hases} = 1;
-					}
-					#$entry->{emitme} = 0;
 					#$entry->{lang} = {};
 					#$entry->{lang}->{label} = $langname;
 					#if (exists $langnamestocodes{$langname}) {
@@ -194,19 +204,84 @@ while (1) {
 					}
 				}
 
-				$prevsection = appendsection($section, $level);
+				$prevsection = appendsection($section);
 			} # Heading
 			
+			# Not heading, just plain lines
 			else {
-				# Not heading, just plain lines
 				push @{$section->{lines}}, $tline;
 			}
 
 			last unless ($xline = <STDIN>);
 		} # while (1)
 
+		# Process raw section tree into a more structured entry
+		foreach my $p ($page->{raw}->{sections}[0]) {
+			foreach my $lang (@{$p->{sections}}) {
+				if ($lang->{heading} =~ /$lang_pat/o) {
+					foreach my $l3 (@{$lang->{sections}}) {
+						# TODO handle l3=Etymology # + l4=Noun
+						if ($l3->{heading} eq 'Noun') {
+							my $w = $p->{heading};
+							my $l = $l3->{lines}[0];
+
+							# TODO should be a loop
+							if ($l3->{lines}[1]) {
+								if ($l eq '' || $l =~ /^{{wikipediaAlt\|/ || $l =~ /^\[\[Category:[^]]*]]/) {
+									$l = $l3->{lines}[1];
+								}
+							}
+
+							my $g = undef;
+							my $n = undef;
+
+							++$tried_to_parse;
+
+#							if ($l =~ /^'''.*?''',?\s+{{(m|f|n|c|mf|fm)(\.?pl\.?)?}}/) {
+#								$g = $1;
+#								$n = 'p' if ($2);
+#							} elsif ($l =~ /^'''.*?''',?\s+''([mfnc])\.?''/) {
+#								$g = $1;
+#							} elsif ($l =~ /^'''.*?'''\s+''([mfnc]), ?pl(?:ural)?''(.*)/) {
+#								$g = $1;
+#								$n = 'p' unless ($2);
+#							} elsif ($l =~ /^'''.*?'''\s+''([mfnc]) ?pl(?:ural)?''/) {
+#								$g = $1;
+#								$n = 'p';
+#							} elsif ($l =~ /^'''.*?'''\s+''([mfnc])(?:\/|, )([mfnc])''/) {
+#								$g = $1 . $2;
+#							} elsif ($l =~ /^'''.*?'''\s*$/) {
+#								$g = '-';
+#							# Careful - this one is actually for "mf" pairs (not invariants)
+#							} elsif ($l =~ /^{{$lang_code-noun-mf\|f(?:emale)?=/o) {
+#								$g = 'm';
+#							} elsif ($l =~ /^{{$lang_code-noun-(m|f|n|c|mf|fm)\b/o) {
+#								$g = $1;
+#							} elsif ($l =~ /^{{$lang_code-noun2?\|(m|f|n|c|mf|fm)\b/o) {
+#								$g = $1;
+#							} elsif ($l =~ /^{{$lang_code-noun2?\|g(?:ender)?=(m|f|n|c|mf|fm)\b/o) {
+#								$g = $1;
+#							} elsif ($l =~ /^{{infl\|$lang_code\|noun(?:\|g(?:ender)?=([mfnc]))?\b/o) {
+#								$g = $1 ? $1 : '-';
+#							} elsif ($l =~ /^'''.*?'''\s+\((?:'')?plural:?(?:'')?:? '''.*?'''\)(?:\s+{{([mfnc])}})?/) {
+#								$g = $1 ? $1 : '-';
+#							}
+#							if ($g) {
+#								++$parsed_ok;
+#								print "$w :: $g\n";
+#							} else {
+#								print "$w : $l\n";
+#							}
+							print STDERR "$w ($parsed_ok / $tried_to_parse)\n";
+						}
+					}
+					last;	# Skip the following language entries
+				}
+			}
+		}
+
 		# Emit page
-		emitsection($page->{raw}->{sections}[0]);
+		#emitsection($page->{raw}->{sections}[0]);
 	}
 
 	# Skip remainder of page
@@ -214,14 +289,14 @@ while (1) {
 		last if ($xline =~ /<\/page>/);
 	}
 	if ($xline !~ /<\/page>/) {
-		print STDERR "unexpected end of file in $title\n";
+		print STDERR "** unexpected end of file in $title\n";
 		exit;
 	}
 
 	# Enough pages processed?
 	if ($ns eq '') {
 		if ($maxpages != 0 && $pagecounter >= $maxpages) {
-			print STDERR "max number of pages parsed\n";
+			print STDERR "** max number of pages parsed\n";
 			print "</wiktionary>\n";
 			exit;
 		}
@@ -257,12 +332,12 @@ sub emitsection {
 #
 #
 sub appendsection {
-	my ($section, $level) = @_;
+	my $section = shift;
 
-	$section->{parent} = @scope[$level - 1];
-	push @{@scope[$level - 1]->{sections}}, $section;
+	$section->{parent} = @scope[$section->{level} - 1];
+	push @{@scope[$section->{level} - 1]->{sections}}, $section;
 
-	for (my $l = $level; $l <= 7; ++$l) {
+	for (my $l = $section->{level}; $l <= 7; ++$l) {
 		@scope[$l] = $section;
 	}
 
