5330dc92bf9d1f150a57d5adc550fcbff6bb306a
hippietrail <hippietrail@5d201f8b-831d-0410-bf51-e7b6101be352>
2010-01-04 07:28:22 +0000

    add command line option to process a dump without downloading it first
    git-svn-id: https://svn.toolserver.org/svnroot/p_enwikt@172 5d201f8b-831d-0410-bf51-e7b6101be352

--- wiktdump/newdump.sh
+++ wiktdump/newdump.sh
@@ -1,4 +1,31 @@
-wget -O - http://download.wikipedia.org/enwiktionary/20091230/enwiktionary-20091230-pages-articles.xml.bz2 | bzip2 -d > /mnt/user-store/enwiktionary-20091230-pages-articles.xml & wget -O - http://download.wikimedia.org/enwiktionary/20091230/enwiktionary-20091230-categorylinks.sql.gz | gzip -cd > /mnt/user-store/enwiktionary-20091230-categorylinks.sql
+#
+# download and index new English Wiktionary dump
+#
+# creates low level indeces then high level ones for the database to import
+#
+# -G : don't wget and un- bzip2/gzip dump files, assume they are in /mnt/user-store/
+#
+
+wget_and_unzip=1
+
+if [ $# -eq 1 ] ; then
+    if [ $1 = '-G' ] ; then
+        wget_and_unzip=0
+    else
+        echo "unknown command line options" >&2
+        exit 1
+    fi
+fi
+
+if [ $wget_and_unzip -eq 1 ] ; then
+    echo "get and unzip..."
+else
+    echo "index existing dump files...";
+fi
+
+if [ $wget_and_unzip -eq 1 ] ; then
+    wget -O - http://download.wikipedia.org/enwiktionary/20091230/enwiktionary-20091230-pages-articles.xml.bz2 | bzip2 -d > /mnt/user-store/enwiktionary-20091230-pages-articles.xml & wget -O - http://download.wikimedia.org/enwiktionary/20091230/enwiktionary-20091230-categorylinks.sql.gz | gzip -cd > /mnt/user-store/enwiktionary-20091230-categorylinks.sql
+fi
 
 nice -n 24 perl wiktdump/wiktmkrawidx.pl /mnt/user-store/enwiktionary-20091230-pages-articles.xml > /mnt/user-store/en20091230-off.raw ; nice -n 24 perl wiktdump/wiktallnames.pl /mnt/user-store/en20091230-off.raw /mnt/user-store/enwiktionary-20091230-pages-articles.xml | tee >(nice -n 24 perl wiktdump/wiktmkbsearchidx.pl - > /mnt/user-store/en20091230-all-off.raw) >(nice -n 24 perl wiktdump/wiktmksortedidx.pl - > /mnt/user-store/en20091230-all-idx.raw) > /mnt/user-store/en20091230-all.txt
 
