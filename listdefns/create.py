ce2ede5cd2a911998dc737e91446947e0f62b758
conrad <conrad@5d201f8b-831d-0410-bf51-e7b6101be352>
2010-05-13 13:56:11 +0000

    Don't remove "bad" definitions here, let consumers.
    git-svn-id: https://svn.toolserver.org/svnroot/p_enwikt@211 5d201f8b-831d-0410-bf51-e7b6101be352

--- listdefns/create.py
+++ listdefns/create.py
@@ -3,18 +3,16 @@ import wiktionary
 import codecs
 import sys
 
-if len(sys.argv) > 2:
-    with codecs.open(sys.argv[1], 'w', 'utf8') as fgood:
-        with codecs.open(sys.argv[2], 'w', 'utf8') as fbad:
-            for entry in wiktionary.dump_entries(main_only=True):
-                for lang in wiktionary.language_sections(entry.text):
+if len(sys.argv) == 2:
+    with codecs.open(sys.argv[1], 'w', 'utf8') as output:
+        for entry in wiktionary.dump_entries(main_only=True):
+            for lang in wiktionary.language_sections(entry.text):
+                if lang.heading:
                     for sect in wiktionary.all_subsections(lang.text):
                         for defn in wiktionary.definition_lines(sect.text):
-                            print >>(fgood if wiktionary.is_part_of_speech(sect.heading) else fbad),\
-                                "%s\t%s\t%s\t%s" % (lang.heading, entry.name, sect.heading, defn)
-
+                            print >>output,\
+                                ("%s\t%s\t%s\t%s" % (lang.heading, entry.name, sect.heading, defn))
 else:
-    print "Usage: %s <output> <errors>" % sys.argv[0]
+    print "Usage: %s <output> " % sys.argv[0]
     print "Creates a tab-separated-file:"
     print "Language\tWord\tPart-of-speech\tDefinition"
-    print "Valid parts of speech can be tweaked in wiktionary.py"
--- listdefns/create.sh
+++ listdefns/create.sh
@@ -3,15 +3,19 @@
 path=`dirname $0`
 date=`$path/latest_dump.sh | tr -d -c '0-9'`
 
-if [ ! -f ~enwikt/public_html/definitions/enwikt-defs-$date-all.tsv.gz ]
+if [ "x--force" = "x$1" -o ! -f ~enwikt/public_html/definitions/enwikt-defs-$date-all.tsv.gz ]
 then
-	nice python $path/create.py $path/TEMP$date.tsv $path/errors/enwikt-badhashes-$date.tsv
-	nice sort $path/TEMP$date.tsv > $path/TEMP-S$date.tsv
-	nice grep '^English\b' $path/TEMP-S$date.tsv > $path/TEMP-E$date.tsv
-	nice gzip $path/TEMP-S$date.tsv
-	nice gzip $path/TEMP-E$date.tsv
-	mv $path/TEMP-S$date.tsv.gz ~enwikt/public_html/definitions/enwikt-defs-$date-all.tsv.gz
-	mv $path/TEMP-E$date.tsv.gz ~enwikt/public_html/definitions/enwikt-defs-$date-en.tsv.gz
-	ln -f ~enwikt/public_html/definitions/enwikt-defs-$date-all.tsv.gz ~enwikt/public_html/definitions/enwikt-defs-all-latest.tsv.gz
-	ln -f ~enwikt/public_html/definitions/enwikt-defs-$date-en.tsv.gz ~enwikt/public_html/definitions/enwikt-defs-en-latest.tsv.gz
+	nice python $path/create.py $path/TEMP$date.tsv
+	if [ "`head $path/TEMP$date.tsv`" ]
+	then
+		nice sort $path/TEMP$date.tsv > $path/TEMP-S$date.tsv
+		nice grep '^English\b' $path/TEMP-S$date.tsv > $path/TEMP-E$date.tsv
+		nice gzip $path/TEMP-S$date.tsv
+		nice gzip $path/TEMP-E$date.tsv
+		mv $path/TEMP-S$date.tsv.gz ~enwikt/public_html/definitions/enwikt-defs-$date-all.tsv.gz
+		mv $path/TEMP-E$date.tsv.gz ~enwikt/public_html/definitions/enwikt-defs-$date-en.tsv.gz
+		ln -f ~enwikt/public_html/definitions/enwikt-defs-$date-all.tsv.gz ~enwikt/public_html/definitions/enwikt-defs-latest-all.tsv.gz
+		ln -f ~enwikt/public_html/definitions/enwikt-defs-$date-en.tsv.gz ~enwikt/public_html/definitions/enwikt-defs-latest-en.tsv.gz
+		rm $path/TEMP$date.tsv
+	fi
 fi
