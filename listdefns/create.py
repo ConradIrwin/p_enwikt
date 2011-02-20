f62998348b3d8ce90b2722533c7b65078e755f5e
conrad <conrad@5d201f8b-831d-0410-bf51-e7b6101be352>
2011-02-20 01:42:37 +0000

    Remove tabs more explicitly
    git-svn-id: https://svn.toolserver.org/svnroot/p_enwikt@223 5d201f8b-831d-0410-bf51-e7b6101be352

--- listdefns/create.py
+++ listdefns/create.py
@@ -3,6 +3,9 @@ import wiktionary
 import codecs
 import sys
 
+def detab(string):
+	return string.replace(u"\t", u"        ")
+
 if len(sys.argv) == 3:
     with codecs.open(sys.argv[2], 'w', 'utf8') as output:
         for entry in wiktionary.dump_entries(main_only=True, dump=sys.argv[1]):
@@ -11,7 +14,7 @@ if len(sys.argv) == 3:
                     for sect in wiktionary.all_subsections(lang.text):
                         for defn in wiktionary.definition_lines(sect.text):
                             print >>output,\
-                                ("%s\t%s\t%s\t%s" % (lang.heading, entry.name, sect.heading, defn))
+				"\t".join(detab(field) for field in (lang.heading, entry.name, sect.heading, defn))
 else:
     print "Usage: %s <dump> <output> " % sys.argv[0]
     print "Creates a tab-separated-output-file from an English Wiktionary XML dump:"
