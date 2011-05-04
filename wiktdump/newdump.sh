915bf6d6a6a45428a61eb6bffc41654563012051
hippietrail <hippietrail@5d201f8b-831d-0410-bf51-e7b6101be352>
2011-05-04 06:53:12 +0000

    add -c / -C switches to enable / disable running wiktstruct.pl (it doesn't support the new dump index format yet)
    git-svn-id: https://svn.toolserver.org/svnroot/p_enwikt@251 5d201f8b-831d-0410-bf51-e7b6101be352

--- wiktdump/newdump.sh
+++ wiktdump/newdump.sh
@@ -5,19 +5,23 @@
 #
 # -G : don't wget and un- bzip2/gzip dump files, assume they are in /mnt/user-store/
 # -L : don't do low-level indexing of dump files, assume those files have already been created in /mnt/user-store/
+# -C : don't run wiktstruct.pl
 #
 
 date=
 wget_and_unzip=1
 low_level_index=1
+wikt_struct_pl=1
 
-while getopts gGlL opt
+while getopts gGlLcC opt
 do
     case "$opt" in
         g)  wget_and_unzip=1;;
         G)  wget_and_unzip=0;;
         l)  low_level_index=1;;
         L)  low_level_index=0;;
+        c)  wikt_struct_pl=1;;
+        C)  wikt_struct_pl=0;;
     esac
 done
 shift `expr $OPTIND - 1`
@@ -42,6 +46,12 @@ else
     echo "won't create low-level index of dump files..."
 fi
 
+if [ $wikt_struct_pl -eq 1 ] ; then
+    echo "will run wiktstruct.pl..."
+else
+    echo "won't run wiktstruct.pl..."
+fi
+
 echo "getting and unzipping pages and categories..."
 
 if [ $wget_and_unzip -eq 1 ] ; then
@@ -60,6 +70,8 @@ else
     echo "page and cat both ok";
 fi
 
+# TODO replace with the new C version: indexwiki
+
 if [ $low_level_index -eq 1 ] ; then
     nice -n 24 perl wiktdump/wiktmkrawidx.pl /mnt/user-store/enwiktionary-$date-pages-articles.xml > /mnt/user-store/en$date-off.raw ; nice -n 24 perl wiktdump/wiktallnames.pl /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml | tee >(nice -n 24 perl wiktdump/wiktmkbsearchidx.pl - > /mnt/user-store/en$date-all-off.raw) >(nice -n 24 perl wiktdump/wiktmksortedidx.pl - > /mnt/user-store/en$date-all-idx.raw) > /mnt/user-store/en$date-all.txt
 fi
@@ -70,8 +82,11 @@ rm -rf buxxo fluxxo
 mkdir buxxo; mkdir fluxxo
 rm __*
 
-nice -n 24 perl ../wiktdump/wiktsplitnames.pl /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml & nice -n 24 perl ../wiktdump/wiktsplitnames.pl -b /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml & nice -n 24 perl ../wiktdump/wiktstruct.pl /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml
-#nice -n 24 perl ../wiktdump/wiktsplitnames.pl /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml & nice -n 24 perl ../wiktdump/wiktstruct.pl /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml
+if [ $wikt_struct_pl -eq 1 ] ; then
+    nice -n 24 perl ../wiktdump/wiktsplitnames.pl /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml & nice -n 24 perl ../wiktdump/wiktsplitnames.pl -b /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml & nice -n 24 perl ../wiktdump/wiktstruct.pl /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml
+else
+    nice -n 24 perl ../wiktdump/wiktsplitnames.pl /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml & nice -n 24 perl ../wiktdump/wiktsplitnames.pl -b /mnt/user-store/en$date-off.raw /mnt/user-store/enwiktionary-$date-pages-articles.xml
+fi
 
 cd ..
 
